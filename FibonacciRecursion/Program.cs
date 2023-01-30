96bf0079d1e708739ef7955437edaf547bd178e8
Leila Schmidt <lnsnani80@gmail.com>
2023-01-30 14:12:11 +0100

    using the for loop instead of recursion

--- FibonacciRecursion/Program.cs
+++ FibonacciRecursion/Program.cs
@@ -2,30 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, how many numbers of the fibonacci sequence would you like to receive?");
             var howMany = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("you'd like to receive " + howMany + " back.");
+            int[] arr = FibonaciCalculations(howMany);
+
+            Console.WriteLine(string.Join(",", arr));
         }
 
-        static int FibonaciCalculations(int howMany)
+        static int[] FibonaciCalculations(int howMany)
         {
-            int count = 0;
-            while (count < howMany)
+            int[] fibonacci = new int[howMany];
+            fibonacci[0] = 1;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < howMany; i++)
             {
-                //declaration
-                int i = 0;
-                int j = i - 1;
-                int[] fibonacci = new int[i];
-
-
-                int next = fibonacci[i] + fibonacci[j];
-                FibonaciCalculations(i);
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
             }
-
-            return howMany;
+            return fibonacci;
         }
     }
 }
