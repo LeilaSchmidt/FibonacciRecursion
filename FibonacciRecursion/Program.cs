ebd36b3f7d4182199e6b6f491f8f13feee566d79
Leila Schmidt <le.schmidt@douglas.de>
2023-01-30 12:46:57 +0100

    Add project files.

---
+++ FibonacciRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FibonacciRecursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, how many numbers of the fibonacci sequence would you like to receive?");
+            var howMany = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("you'd like to receive " + howMany + " back.");
+        }
+
+        static int FibonaciCalculations(int howMany)
+        {
+            int count = 0;
+            while (count < howMany)
+            {
+                //declaration
+                int i = 0;
+                int j = i - 1;
+                int[] fibonacci = new int[i];
+
+
+                int next = fibonacci[i] + fibonacci[j];
+                FibonaciCalculations(i);
+            }
+
+            return howMany;
+        }
+    }
+}
